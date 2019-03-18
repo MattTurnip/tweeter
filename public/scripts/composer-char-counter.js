02a56bd35243f0d90b7fec3312e2054addb31efd
MattTurnip <mattadavey@gmail.com>
2019-03-18 22:14:44 +0000

    Styled nav and tweet section, Implemented jquery tweet counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function () {
+    console.log("fully loaded, jquery ready ;\)")
+});
+
+$("textarea").on("input", function () {
+    let charCount = 140 - this.value.length;
+    if (charCount > 0) {
+        $(".counter").text(charCount).removeClass("red-text");
+    } else if (charCount === 0) {
+        $(".counter").text(charCount);
+    } else {
+        $(".counter").html(charCount).addClass("red-text");
+    }
+});
