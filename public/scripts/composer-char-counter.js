179699599a12f939b37dc0f12faf0ce3b0f91ad3
MattTurnip <mattadavey@gmail.com>
2019-03-19 22:28:35 +0000

    Add jquery document ready

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,18 @@
 
 $(document).ready(function () {
-    console.log("fully loaded, jquery ready ;\)")
-});
+    console.log("fully loaded, jquery ready ;\)");
 
-$("textarea").on("input", function () {
-    let charCount = 140 - this.value.length;
-    if (charCount > 0) {
-        $(".counter").text(charCount).removeClass("red-text");
-    } else if (charCount === 0) {
-        $(".counter").text(charCount).removeClass("red-text");
-    } else {
-        $(".counter").html(charCount).addClass("red-text");
-    }
+
+    $("textarea").on("input", function () {
+        let charCount = 140 - this.value.length;
+        if (charCount > 0) {
+            $(".counter").text(charCount).removeClass("red-text");
+        } else if (charCount === 0) {
+            $(".counter").text(charCount).removeClass("red-text");
+        } else {
+            $(".counter").html(charCount).addClass("red-text");
+        }
+    });
+
+
 });
