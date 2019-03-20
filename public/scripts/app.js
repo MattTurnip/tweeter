7558f26f38e64c36029fa4ac8b6f3c1fc46472a1
MattTurnip <mattadavey@gmail.com>
2019-03-20 17:49:51 +0000

    Add escape function for XSS, load new tweets upon post, alerts for invalid textarea

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,22 @@
 $(document).ready(function () {
-
+    function escape(str) {
+        var div = document.createElement('div');
+        div.appendChild(document.createTextNode(str));
+        return div.innerHTML;
+    }
 
     function createTweetElement(tweet) {
         const output = `
         <article class="tweet">
         <header>
-          <img src="${tweet.user.avatars.large}" alt="${tweet.user.name}'s avatar">
-          <h2>${tweet.user.name}</h2><span>${tweet.user.handle}</span>
+          <img src="${escape(tweet.user.avatars.large)}" alt="${escape(tweet.user.name)}'s avatar">
+          <h2>${escape(tweet.user.name)}</h2><span>${escape(tweet.user.handle)}</span>
         </header>
         <div>
-          <p>${tweet.content.text}</p>
+          <p>${escape(tweet.content.text)}</p>
         </div>
         <footer>
-          ${tweet.created_at}
+          ${escape(tweet.created_at)}
           <span class="favicons">
             <i class="fas fa-heart"></i><i class="fas fa-flag"></i><i class="fas fa-retweet"></i>
           </span>
@@ -38,12 +42,15 @@ $(document).ready(function () {
         if ($tweetText.length <= 140 && $tweetText.length > 0) {
             const $tweetToPost = $("textarea").serialize();
             console.log("your serialized tweet:", $tweetToPost);
-            $.ajax("/tweets/", { method: "POST", data: $tweetToPost });
-        } else if ($tweetText === "" || $tweetText === null) {
+            $.ajax("/tweets/", { method: "POST", data: $tweetToPost })
+                .success(() => {
+                    loadTweets()
+                });
+        } else if ($tweetText === "" || $tweetText === null || $tweetText.length > 140) {
             $("textarea").attr("placeholder", "Please enter something between 1 and 140 characters");
-            alert("enter something between 1 and 140 chars")
+            alert("enter something between 1 and 140 chars");
         } else {
-            console.log("be afraid");
+            console.log("congrats on finding a new edge case");
         }
 
 
